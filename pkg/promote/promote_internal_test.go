b9f1bed7c8368436e24dec60d978d5c3c2c05478
James Skeoch <jaa.skeoch@gmail.com>
2022-02-16 10:41:24 +0000

    test: add checkPullRequestStatus

--- pkg/promote/promote_internal_test.go
+++ pkg/promote/promote_internal_test.go
@@ -1,13 +1,17 @@
 package promote
 
 import (
+	"bytes"
+	"context"
 	"github.com/jenkins-x-plugins/jx-promote/pkg/environments"
 	"github.com/jenkins-x/go-scm/scm"
 	scmFake "github.com/jenkins-x/go-scm/scm/driver/fake"
 	jxFake "github.com/jenkins-x/jx-api/v4/pkg/client/clientset/versioned/fake"
+	"github.com/jenkins-x/jx-logging/v3/pkg/log"
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	kubeFake "k8s.io/client-go/kubernetes/fake"
+	"os"
 	"testing"
 )
 
@@ -132,6 +136,125 @@ func TestOptions_prIsMergedWithSHA(t *testing.T) {
 	for _, testCase := range testCases {
 		t.Run(testCase.name, func(t *testing.T) {
 			assert.Equal(t, testCase.expectedBool, prIsMergedWithSHA(testCase.args.pr))
+		})
+	}
+}
+
+func TestOptions_checkPullRequestStatus(t *testing.T) {
+	type arguments struct {
+		pr              *scm.PullRequest
+		ctx             context.Context
+		repo            scm.Repository
+		prInfo          *scm.PullRequest
+		logMergeFailure bool
+	}
+
+	testCases := []struct {
+		name          string
+		testOptions   Options
+		args          arguments
+		state         scm.State
+		expectedError string
+		expectedLog   string
+	}{
+		//{
+		//	name:        "PullRequestLastCommitStatus_Fail",
+		//	testOptions: Options{},
+		//	args: arguments{
+		//		pr:              &scm.PullRequest{},
+		//		repo:            scm.Repository{},
+		//		logMergeFailure: false,
+		//	},
+		//	expectedError: "",
+		//	expectedLog:   "WARNING: Failed to query the Pull Request last commit status for  ref  no ScmClient\n",
+		//},
+		{
+			name: "State is Pending",
+			testOptions: Options{
+				KubeClient: kubeFake.NewSimpleClientset(),
+			},
+			args: arguments{
+				pr:              &scm.PullRequest{},
+				repo:            scm.Repository{},
+				logMergeFailure: false,
+			},
+			state:         scm.StatePending, // - Pending
+			expectedError: "",
+			expectedLog:   "The build for the Pull Request last commit is currently in progress.\n",
+		},
+		{
+			name: "State is Success",
+			testOptions: Options{
+				KubeClient:         kubeFake.NewSimpleClientset(),
+				NoMergePullRequest: true,
+			},
+			args: arguments{
+				pr:              &scm.PullRequest{},
+				repo:            scm.Repository{},
+				logMergeFailure: false,
+			},
+			state:         scm.StateSuccess, // - Pending
+			expectedError: "",
+			expectedLog:   "",
+		},
+		{
+			name: "State is Failure",
+			testOptions: Options{
+				KubeClient:         kubeFake.NewSimpleClientset(),
+				NoMergePullRequest: true,
+			},
+			args: arguments{
+				pr:              &scm.PullRequest{},
+				repo:            scm.Repository{},
+				logMergeFailure: false,
+			},
+			state:         scm.StateFailure, // - Failure
+			expectedError: "pull request  last commit has status failure for ref ",
+			expectedLog:   "",
+		},
+		{
+			name: "State is Unknown",
+			testOptions: Options{
+				KubeClient:         kubeFake.NewSimpleClientset(),
+				NoMergePullRequest: true,
+			},
+			args: arguments{
+				pr:              &scm.PullRequest{},
+				repo:            scm.Repository{},
+				logMergeFailure: false,
+			},
+			state:         scm.StateUnknown, // - Unknown
+			expectedError: "",
+			expectedLog:   "got git provider status unknown from PR \n",
+		},
+	}
+
+	for _, testCase := range testCases {
+		// Build new fake SCM client for test
+		scmFakeClient, scmFakeData := scmFake.NewDefault()
+		scmFakeData.PullRequests[1] = testCase.args.pr
+		testCase.testOptions.ScmClient = scmFakeClient
+		scmFakeData.Statuses[""] = []*scm.Status{
+			{
+				State: testCase.state,
+			},
+		}
+
+		t.Run(testCase.name, func(t *testing.T) {
+			// Set logger to output to buffer to check logs
+			var buf bytes.Buffer
+			log.SetOutput(&buf)
+			defer func() {
+				log.SetOutput(os.Stderr)
+			}()
+
+			err := testCase.testOptions.checkPullRequestStatus(testCase.args.pr, testCase.args.ctx, testCase.args.repo, testCase.args.prInfo, testCase.args.logMergeFailure)
+			if testCase.expectedError == "" {
+				assert.Equal(t, testCase.expectedLog, buf.String())
+				assert.NoError(t, err)
+				return
+			}
+			assert.Equal(t, err.Error(), testCase.expectedError)
 		})
 	}
 }
